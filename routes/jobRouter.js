e9dd43a6c494bde72863ec0715853ec4f4cdf13d
vova-kliuf <vova.kliuf@gmail.com>
2026-06-17 17:37:05 +0300

    v1 - just updates (#1)

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -4,11 +4,13 @@ const { getJobs, newJob, getJobInRadius, updateJob, deleteJob, getJob, getStats,
 const { isAuthenticated, authorizeRoles } = require('../middleware/auth');
 
 router.route('/jobs').get(getJobs);
-router.route('/jobs/:id/:slug').get(getJob);
-router.route('/jobs/new').post(isAuthenticated, authorizeRoles('admin', 'employer'), newJob);
-router.route('/jobs/:zipcode/:distance').get(getJobInRadius);
-router.route('/jobs/:id').put(isAuthenticated, authorizeRoles('admin', 'employer'), updateJob).delete(isAuthenticated, authorizeRoles('admin', 'employer'), deleteJob);
 router.route('/stats/:topic').get(getStats);
+router.route('/jobs/by-slug/:id/:slug').get(getJob);
+router.route('/jobs/by-zipcode/:zipcode/:distance').get(getJobInRadius);
+
+router.route('/jobs/new').post(isAuthenticated, authorizeRoles('admin', 'employer'), newJob);
+
+router.route('/jobs/:id').put(isAuthenticated, authorizeRoles('admin', 'employer'), updateJob).delete(isAuthenticated, authorizeRoles('admin', 'employer'), deleteJob);
 router.route('/jobs/:id/apply').put(isAuthenticated, authorizeRoles('user'), applyForAJob);
 
 module.exports = router;
